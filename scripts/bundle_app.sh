64dfbab9a2afb8a8d8b2b5ae9f47275b5eb8b565
James Fredericks <jamesfredericks@outlook.com>
2026-02-16 12:04:03 -0500

    chore: Bump version to 1.5.0

--- scripts/bundle_app.sh
+++ scripts/bundle_app.sh
@@ -3,7 +3,7 @@
 # Configuration
 APP_NAME="SSHoebox"
 BUNDLE_ID="com.sshoebox.app"
-VERSION="1.4.0"
+VERSION="1.5.0"
 BUILD_DIR=".build/release"
 DIST_DIR="dist"
 APP_BUNDLE="${DIST_DIR}/${APP_NAME}.app"
@@ -76,7 +76,7 @@ cat > "${APP_BUNDLE}/Contents/Info.plist" <<PLIST
     <key>CFBundleShortVersionString</key>
     <string>${VERSION}</string>
     <key>CFBundleVersion</key>
-    <string>5</string>
+    <string>6</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>NSHighResolutionCapable</key>
